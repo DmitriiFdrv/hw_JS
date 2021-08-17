3e3b795d1b9e255d879cfaad91e87bae173d5573
DmitriiFedorov <fdorov_dima@mail.ru>
2021-08-17 21:51:03 +0500

    hw1

---
+++ hw_1/hw_1.js
@@ -0,0 +1,14 @@
+{
+    var tc = prompt('введите градусы по цельсию');
+    tc = parseInt(tc);
+    tf = (9 / 5) * tc + 32;
+    alert('По фаренгейту:' + tf);
+}
+
+{
+    var name = "Василий";
+    admin = name;
+    alert(admin);
+}
+
+
